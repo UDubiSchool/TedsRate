21db7403508fa36773e61c9ef0047a47ba331650
William Menten-Weil <wtmenten@gmail.com>
2015-11-17 00:04:42 -0800

    styling table

--- admin_rp_liam.php
+++ admin_rp_liam.php
@@ -97,9 +97,9 @@ try {
                     <td>{{rating.scenario}}</td>
                     <td>{{rating.userprofile}}</td>
                     <td>
-                        <div ng-if="rating.complete === 'true'">Completed at {{rating.comletionDate}}</div>
+                        <div ng-if="rating.complete === 'true'">Completed at {{rating.completionDate}}</div>
                         <div ng-if="rating.complete !== 'true'">
-                            <button class='email_sender btn btn-primary btn-sm' data-target='#emailModal' data-email='{{rating.email}}' data-urpid='{{rating.urpID}}'>Send Invitation</button>
+                            <button class='email_sender btn btn-primary btn-sm' data-toggle="modal" data-target='#emailModal' data-email='{{rating.email}}' data-urpid='{{rating.urpID}}'>Send Invitation</button>
                         </div>
                     </td>
                 </tr>
