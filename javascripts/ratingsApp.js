5e3cf73f6676384582a09d14437f1ee3b7e6096a
William Menten-Weil <wtmenten@gmail.com>
2015-11-16 21:59:47 -0800

    fixed app typos

--- javascripts/ratingsApp.js
+++ javascripts/ratingsApp.js
@@ -17,44 +17,4 @@ var app = angular.module('dawgCoffee')
 
       $scope.ratings = $rating.get();
 
-   }])
-
-   // .config(function($stateProvider, $urlRouterProvider, $locationProvider) {
-   //   //
-   //   // For any unmatched url, redirect to /state1
-   //   $urlRouterProvider.otherwise("/");
-   //   // $locationProvider.html5Mode(true);
-
-   //   //
-   //   // Now set up the states
-   //   $stateProvider
-   //    .state('Order', {
-   //      url: "/orders",
-   //      templateUrl: "partials/order.html"
-   //    })
-   //    .state('Cart', {
-   //      url: "/orders/cart",
-   //      templateUrl: "partials/cart.html"
-   //    })
-   //    .state('Details', {
-   //      url: "/orders/:beanId",
-   //      templateUrl: "partials/details.html",
-   //      controller : function($scope, $stateParams) {
-   //          $scope.beanId = $stateParams.beanId;
-
-   //       }
-   //    })
-   //     .state('Home', {
-   //       url: "/:jumpPoint",
-   //       templateUrl: "partials/home.html",
-   //       controller : function($scope, $stateParams, $location, $anchorScroll) {
-   //          $location.hash($stateParams.jumpPoint);
-   //          $anchorScroll();
-   //          $location.hash('');
-
-   //       }
-   //     })
-
-    });
-
-  // configure html5 to get links working on jsfiddle
+   }]);
