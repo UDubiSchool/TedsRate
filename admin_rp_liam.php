4fab652aa3bc624aa32304447f94b595ffbe673b
William Menten-Weil <wtmenten@gmail.com>
2015-11-16 22:07:08 -0800

    fixed app typos

--- admin_rp_liam.php
+++ admin_rp_liam.php
@@ -15,7 +15,7 @@ try {
 
 ?>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/angular.js/1.5.0-beta.1/angular.min.js" type="text/javascript"></script>
-<script src="javascripts/ratingsApp.js" type="text/javascript"></script>
+<script src="javascripts/app.js" type="text/javascript"></script>
 <div id="wrapper">
      <?php
         include "nav_part.inc.php";
