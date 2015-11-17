d6e4d8f738c890c40efca5acc547fa6ab15c85f1
William Menten-Weil <wtmenten@gmail.com>
2015-11-16 23:10:30 -0800

    fixed app table template

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,26 +1,9 @@
 var app = angular.module('ratingsApp', [])
-   .service("rating",['$http', function($http) {
-
-      this.ratings = [];
-
-      this.get = function() {
-        // var ratings = [];
-        $http.get("models/admin_rp_model.php").then(function(response) {
-          // var temp = JSON.parse(response.data);
-          this.ratings = response;
-          console.log(response);
-        });
-        console.log(this.ratings);
-        return this.ratings;
-      }
-
-   }])
-
    .controller('MainController', ['$scope', '$http', 'rating' , function($scope, $http, $rating) {
 
       $http.get("models/admin_rp_model.php").then(function(response) {
         // var temp = JSON.parse(response.data);
-        $scope.ratings = response;
+        $scope.ratings = response.data;
         console.log(response);
       });
       console.log($scope.ratings);
