2ff15033e6ed6a7931a7ca6aa3995c32017127df
William Menten-Weil <wtmenten@gmail.com>
2016-01-18 17:35:09 -0800

    rater alteration

--- process.php
+++ process.php
@@ -110,9 +110,7 @@ try {
                       }
 
                       if(empty($errors)==true){
-                        $root = getcwd();
                         $path = "/upload/screenshots/".$file_name;
-                        $path = $root.$path;
                         if(move_uploaded_file($file_tmp, $path)) {
                           $screen_sql = "INSERT INTO screenshot (screenshotPath, userCreated) VALUES ('$path' , " . $ids['user'] . ")";
 
