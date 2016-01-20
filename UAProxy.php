1be085cd08ceb6148a0a1e182df4cd573aaeac39
William Menten-Weil <wtmenten@gmail.com>
2016-01-19 19:44:21 -0800

    cleaning

--- UAProxy.php
+++ UAProxy.php
@@ -1,13 +1,10 @@
 <?php
     if($_GET['target'] && $_GET['ua']) {
 
-        // ini_set("user_agent", $_GET['ua']);
-        // echo '<iframe src="' . $_GET['target'] . '" frameborder="0" width="' . $_GET['w'] . '" height="' . $_GET['h'] . '"></iframe>';
         $ch=curl_init();
         $agent = $_GET['ua'];
         $url = $_GET['target'];
         $timeout = 10;
-        // echo $agent;
         curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
