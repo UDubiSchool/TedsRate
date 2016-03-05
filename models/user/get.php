b9feced7df9796070cfdcefbabb7a57e7489b036
William Menten-Weil <wtmenten@gmail.com>
2016-03-04 17:35:11 -0800

    login issue

--- models/user/get.php
+++ models/user/get.php
@@ -6,8 +6,8 @@ $password = $_POST['password'];
 $getUser = $dbq->prepare("SELECT * FROM user u
                                                 JOIN user_authority ua ON u.userID = ua.userID
                                                 JOIN authority a ON a.authorityID = ua.authorityID
-                                                WHERE email=':email'
-                                                AND passwordValue=':password'
+                                                WHERE u.email=:email
+                                                AND u.passwordValue=:password
                                                 AND a.authorityLevel != 2");
 $getUser->bindValue(':email', $email, PDO::PARAM_STR);
 $getUser->bindValue(':password', $password, PDO::PARAM_STR);
