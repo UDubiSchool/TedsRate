db667908139e8188e2363be2325966124d2a9d22
William Menten-Weil <wtmenten@gmail.com>
2016-03-04 16:27:58 -0800

    password matching

--- assessment.php
+++ assessment.php
@@ -72,7 +72,7 @@
                                         </span>
                                 </div>
                                 <input class="form-control" type="password" name="password" ng-model="signup.password" placeholder="Password" required ng-model-options="{ debounce: 150 }">
-                                <input class="form-control" type="password" name="confirm" ng-model="signup.confirm" placeholder="Confirm" required ui-validate="{ mismatch: '$value!==password && $value && password  ' }"
+                                <input class="form-control" type="password" name="confirm" ng-model="signup.confirm" placeholder="Confirm" required ui-validate="{ mismatch: '$value!==password  ' }"
                         ui-validate-watch=" 'password' " ng-model-options="{ debounce: 150 }">
                             </div>
                             <input class="hidden" type="submit" value="Sign Up">
