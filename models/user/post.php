305dfb02c273697c22a70d48504674789e98daf1
William Menten-Weil <wtmenten@gmail.com>
2016-03-04 17:54:58 -0800

    login issue

--- models/user/post.php
+++ models/user/post.php
@@ -10,7 +10,7 @@ $addAuthority->bindValue(':userID', $userID, PDO::PARAM_INT);
 $addAuthority->execute();
 $addAuthority->closeCursor();
 
-$updateUser = $dbq->prepare("UPDATE user SET email=:email, passwordValue=:password, authorityID = 3 WHERE userID=:id");
+$updateUser = $dbq->prepare("UPDATE user SET email=:email, passwordValue=:password WHERE userID=:id");
 $updateUser->bindValue(':id', $userID, PDO::PARAM_INT);
 $updateUser->bindValue(':email', $email, PDO::PARAM_STR);
 $updateUser->bindValue(':password', $password, PDO::PARAM_STR);
