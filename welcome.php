9bbb46e22c2e3a0fbb103925359a8b42348ca987
William Menten-Weil <wtmenten@gmail.com>
2016-05-12 13:34:04 -0700

    added static welcome page

---
+++ welcome.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+        <link rel="shortcut icon" type="image/x-icon" href="img/favicon.png">
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <link rel="stylesheet" href="css/font-awesome.min.css">
+        <link rel="stylesheet" href="css/base.css">
+
+        <!-- <link rel="stylesheet" href="css/angular-bootstrap-lightbox.css"> -->
+        <!-- <script src="js/jquery-1.11.0.min.js" type="text/javascript"></script> -->
+        <!-- <script src="js/angular/angular.min.js" type="text/javascript"></script> -->
+        <!-- <base href="/"> -->
+        <!-- <base href="/tedsrate/tedsrate/"> -->
+    </head>
+    <body>
+        <style>
+            body {
+                background-color: #2c3e50;
+
+            }
+
+           /* .header {
+                background-color: #fefefe;
+                margin-bottom: 15px;
+            }*/
+
+            .banner-header {
+                position: absolute;
+                top:0px;
+                left: 15px;
+            }
+
+            .content-wrapper {
+                background-color: #fff;
+                padding:0px;
+                font-size: 1.3em;
+            }
+
+            .banner-img {
+                width: 100%;
+                /*padding-bottom:25px;*/
+            }
+
+            .content {
+                padding:25px 15px;
+                text-align: left;
+            }
+        </style>
+        <div class="clearfix" ng-app="assessmentApp" ng-controller="assessmentController">
+         <!--    <div id="header" class="clearfix header">
+                <div class="clearfix pull-left">
+                </div>
+            </div> -->
+            <div class="container content-wrapper col-xs-10 col-sm-9 col-md-8 col-lg-7 center-block">
+                <h2 class="banner-header">Welcome to TedsRate</h2>
+
+                <img src="img/soccerBall.jpg" alt="Soccer ball image" class="banner-img">
+
+                <div class="content">
+                    <p>
+                        The UW TEDS Soccer Research Project is a research from the University of Washington Information School testing a methodology to measure the usability of iOS (Apple) mobile soccer apps. We would really appreciate your evaluation feedback of the Sounders FC iOS mobile application.
+                    </p>
+                    <p>
+                        The Sounders FC application is one of 11 mobile soccer applications from around the world we are comparing in this study. In addition to the academic study, the analysis and data collected will be anonymized and presented to the Sounders to assist in improving their application.
+                    </p>
+                    <p>
+                        We are analyzing 2 scenarios in this study, “Player Information”, and “Schedule, Results and League”. We’d love your opinion on both scenarios if possible, but if you only do one, that’s fine too.  Each survey takes about 5 minutes to complete. We appreciate you taking the time and your valuable opinion.   The first 25 users who complete both surveys will receive a $5 Amazon gift card. Thanks!
+                    </p>
+                    <p>
+                        Please use the following link to evaluate the "Player Information" features. (Roster, player card, player stats, etc):
+                        <br>
+                         <a href="start.php?c=04a8708c3a481ced13845a30de522486895de0592222c29326d9139ec2b9df25">http://depts.washington.edu/tedsrate/tedsrate/start.php?c=04a8708c3a481ced13845a30de522486895de0592222c29326d9139ec2b9df25</a>
+                    </p>
+                    <p>
+                        Please use the following link to evaluate "Schedule, Results and League" features (Schedule calendar, game stats, recap, the table, etc):
+                        <br>
+                        <a href="start.php?c=09a1b036b82baba3177d83c27c1f7d0beacaac6de1c5fdcc9680c49f638c5fb9">http://depts.washington.edu/tedsrate/tedsrate/start.php?c=09a1b036b82baba3177d83c27c1f7d0beacaac6de1c5fdcc9680c49f638c5fb9</a>
+                    </p>
+                    <p>
+                        No personal data will be shared from these surveys.
+                    </p>
+                    <p>
+                        Thanks!
+                        <br>
+                        <br>
+                        <i>The Teds Team</i>
+                    </p>
+                </div>
+
+            </div>
+        </div>
+    </body>
+</html>
