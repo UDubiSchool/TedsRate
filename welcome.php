33e4fc880cc8daae435cbc87a102c1e0d2e2de72
William Menten-Weil <wtmenten@gmail.com>
2016-05-13 11:34:12 -0700

    made links buttons

--- welcome.php
+++ welcome.php
@@ -61,6 +61,14 @@
                 <img src="img/soccerBall.jpg" alt="Soccer ball image" class="banner-img">
 
                 <div class="content">
+                    <div class="col-xs-6">
+                        <a class="btn btn-primary btn-block" href="start.php?c=04a8708c3a481ced13845a30de522486895de0592222c29326d9139ec2b9df25">Start Player Information</a>
+
+                    </div>
+                    <div class="col-xs-6">
+                        <a class="btn btn-primary btn-block" href="start.php?c=09a1b036b82baba3177d83c27c1f7d0beacaac6de1c5fdcc9680c49f638c5fb9">Start Schedule, Results and League</a>
+                    </div>
+
                     <p>
                         The UW TEDS Soccer Research Project is a research from the University of Washington Information School testing a methodology to measure the usability of iOS (Apple) mobile soccer apps. We would really appreciate your evaluation feedback of the Sounders FC iOS mobile application.
                     </p>
@@ -70,16 +78,14 @@
                     <p>
                         We are analyzing 2 scenarios in this study, “Player Information”, and “Schedule, Results and League”. We’d love your opinion on both scenarios if possible, but if you only do one, that’s fine too.  Each survey takes about 5 minutes to complete. We appreciate you taking the time and your valuable opinion.   The first 25 users who complete both surveys will receive a $5 Amazon gift card. Thanks!
                     </p>
-                    <p>
+                    <!-- <p>
                         Please use the following link to evaluate the "Player Information" features. (Roster, player card, player stats, etc):
                         <br>
-                         <a href="start.php?c=04a8708c3a481ced13845a30de522486895de0592222c29326d9139ec2b9df25">Player Information</a>
                     </p>
                     <p>
                         Please use the following link to evaluate "Schedule, Results and League" features (Schedule calendar, game stats, recap, the table, etc):
                         <br>
-                        <a href="start.php?c=09a1b036b82baba3177d83c27c1f7d0beacaac6de1c5fdcc9680c49f638c5fb9">Schedule, Results and League</a>
-                    </p>
+                    </p> -->
                     <p>
                         No personal data will be shared from these surveys.
                     </p>
